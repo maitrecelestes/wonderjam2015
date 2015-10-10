0039565209a5761392e38485c7b37046e57077a8
maitreceleste <romain.soenen@hei.fr>
2015-10-09 23:59:32 -0400

    v2: amélioration niveau 1+ début niveau 2

--- Assets/Script/EnnemiIA.cs
+++ Assets/Script/EnnemiIA.cs
@@ -3,36 +3,70 @@ using System.Collections;
 
 public class EnnemiIA : MonoBehaviour {
 	private GameObject ennemi;
-	public Vector2 speed = new Vector2(50, 50);
+	private GameObject player;
+	private GameObject newEnnemi;
+	private Vector2 posInit;
+	public Vector2 speed = new Vector2(1, 1);
+	private Vector2 posLeft;
+	private Vector2 posRight;
+	private bool versRight;
+	public bool modePoursuite;
 	private Vector2 movement;
+
 
 	// Use this for initialization
 	void Start () {
-		ennemi = GameObject.Find("Ennemi");
+		ennemi = GameObject.FindGameObjectWithTag ("Ennemi");
+		player = GameObject.FindGameObjectWithTag ("Player");
+		newEnnemi = GameObject.Find("Ennemi");
+		posInit = ennemi.transform.position;
+		posLeft= new Vector2 (posInit.x-1, 0);
+
+		posRight= new Vector2 (posInit.x+1, 0);
+		versRight = false;
 	}
 	
-	// Update is called once per frame
-	void Update () {
-		movement = new Vector2(
-			speed.x,
-			0);
+	// Deplacement des ennemis
+	void Update () { 
+		if (versRight) {
+			versRight=false;
+		} 
+		else if (!versRight){
+			versRight=true;
+		}
+		if (!modePoursuite) {
+			if (versRight) {
+				movement = new Vector2 (
+					speed.x,
+					0);
+
+			} else {
+				movement = new Vector2 (
+					-speed.x,
+					0);
+			}
+		} else {
+			if (ennemi.transform.position.x>player.transform.position.x){
+				movement = new Vector2 (
+					-speed.x,
+					0);
+			} else {
+				movement = new Vector2 (
+					speed.x,
+					0);
+			}
+		}
+
 
 	}
+
 
 	void FixedUpdate()
 	{
 		// 5 - Déplacement
 		GetComponent<Rigidbody2D>().velocity = movement;
+
+
 	}
 
-	void OnCollisionEnter2D(Collision2D coll){
-		float pointCollision = coll.transform.position.y;
-		Vector2 posEnemi = ennemi.transform.position;
-		Debug.Log ("pointCollision"+pointCollision);
-		Debug.Log ("posEnnemi"+posEnemi.y);
-
-		if (coll.gameObject.tag == "Player" && posEnemi.y < pointCollision) {
-			Destroy(ennemi);
-		}
-	}
 }
--- Assets/Script/PlayerScript.cs
+++ Assets/Script/PlayerScript.cs
@@ -13,18 +13,34 @@ public class PlayerScript : MonoBehaviour
 	// 2 - Stockage du mouvement
 	private Vector2 movement;
 	private GameObject PauseCanvas; 
-
-	private float jumpHeight;
+	private GameObject GameOver; 
+	private GameObject player;
 	private bool isjumping= false;
+	int ScoreValue=1;
+	private GameController gameController;
 
 	void Start()
 	{
-		jumpHeight = 20;
+		GameObject gameControllerObject = GameObject.FindWithTag ("GameController");
+		if (gameControllerObject != null) {
+			gameController=gameControllerObject.GetComponent<GameController>();
+		}
+		if (gameControllerObject == null) {
+			Debug.Log("Cannot find 'GameController' script");
+		}
+		player = GameObject.Find ("Heros");
 		PauseCanvas = GameObject.Find ("PauseCanvas");
 		PauseCanvas.SetActive (false);
+		GameOver = GameObject.Find ("GameOver");
+		GameOver.SetActive (false);
 	}
 	void Update()
 	{
+		if (transform.position.y <= -2) {
+			player.SetActive(false);
+			Time.timeScale = 0;
+			GameOver.SetActive(true);
+		}
 		if (transform.position.y <= 0.01118645) {
 			isjumping = false;
 		}
@@ -40,7 +56,7 @@ public class PlayerScript : MonoBehaviour
 			{
 				movement = new Vector2(
 				speed.x * inputX,
-				jumpHeight);
+				20);
 				isjumping=true;
 			}
 
@@ -71,6 +87,22 @@ public class PlayerScript : MonoBehaviour
 	void OnCollisionEnter2D(Collision2D col)
 	{
 		isjumping = false;
+		float pointCollision = player.transform.position.y;
+		if(col.gameObject.CompareTag("Ennemi")){
+			Vector2 posEnemi = col.transform.position;
+			
+		if (posEnemi.y+0.08 < pointCollision) { //On ajoute 0.2 pour prendre en compte la taille de l'ennemi
+				col.gameObject.SetActive(false);
+			
+			gameController.AddScore(ScoreValue);
+		}
+			else
+			{
+				player.SetActive(false);
+				Time.timeScale = 0;
+				GameOver.SetActive(true);
+			}
+		}
 	}
 
 }
